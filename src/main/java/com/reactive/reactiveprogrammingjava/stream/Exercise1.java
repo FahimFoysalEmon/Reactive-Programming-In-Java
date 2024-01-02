3ae99d4a754966c8101efd85ad47703fb0a628a8
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2024-01-02 13:04:23 +0600

    stream exercise with filter

--- src/main/java/com/reactive/reactiveprogrammingjava/stream/Exercise1.java
+++ src/main/java/com/reactive/reactiveprogrammingjava/stream/Exercise1.java
@@ -1,4 +1,23 @@
 package com.reactive.reactiveprogrammingjava.stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        // Print numbers from intNumbersStream that are less than 5
+        numbers.stream().forEach(number -> {
+            System.out.println(number < 5 ? number : ""); // Print if number is less than 5
+        });
+
+
+        // Print the second and third numbers in intNumbersStream that's greater than 5
+        numbers.stream().filter(number -> number>5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+
+    }
 }
