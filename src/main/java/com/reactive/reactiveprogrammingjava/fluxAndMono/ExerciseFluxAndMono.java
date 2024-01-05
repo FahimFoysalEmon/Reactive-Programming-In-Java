bcaa62be6e1d81eac47ce789a8112c52cfe822ea
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2024-01-05 16:24:48 +0600

    flux and mono , new event emitted and subscribe calles it

--- src/main/java/com/reactive/reactiveprogrammingjava/fluxAndMono/ExerciseFluxAndMono.java
+++ src/main/java/com/reactive/reactiveprogrammingjava/fluxAndMono/ExerciseFluxAndMono.java
@@ -2,15 +2,24 @@ package com.reactive.reactiveprogrammingjava.fluxAndMono;
 
 import com.reactive.reactiveprogrammingjava.commons.ReactiveSources;
 
+import java.io.IOException;
+
 public class ExerciseFluxAndMono {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         ReactiveSources.intNumbersFlux().subscribe(System.out::println);
+
+
+        // Print all users in the ReactiveSources.userFlux stream
+        ReactiveSources.userFlux().subscribe(System.out::println);
+
+        System.out.println("Press a key to end");
+        System.in.read();
 
     }
 
