e342a23552a114ceafb26de48174b04cea42fac0
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2024-01-09 17:00:50 +0600

    exercise 2 flux

--- src/main/java/com/reactive/reactiveprogrammingjava/fluxAndMono/ExerciseFluxAndMono1.java
+++ src/main/java/com/reactive/reactiveprogrammingjava/fluxAndMono/ExerciseFluxAndMono1.java
@@ -4,7 +4,7 @@ import com.reactive.reactiveprogrammingjava.commons.ReactiveSources;
 
 import java.io.IOException;
 
-public class ExerciseFluxAndMono {
+public class ExerciseFluxAndMono1 {
 
     public static void main(String[] args) throws IOException {
 
@@ -19,7 +19,7 @@ public class ExerciseFluxAndMono {
 
 
         //subscribe() executes as soon as the print line happens! without an action subscribe will not happen
-
+        //ordering is not deterministic
         System.out.println("Press a key to end\n");
         System.in.read();
 
---
+++ src/main/java/com/reactive/reactiveprogrammingjava/fluxAndMono/ExerciseFluxAndMono2.java
@@ -0,0 +1,23 @@
+package com.reactive.reactiveprogrammingjava.fluxAndMono;
+
+import com.reactive.reactiveprogrammingjava.commons.ReactiveSources;
+
+import java.util.List;
+
+public class ExerciseFluxAndMono2 {
+
+    public static void main(String[] args) {
+        // Use ReactiveSources.intNumbersFlux()
+
+        // Get all numbers in the ReactiveSources.intNumbersFlux stream
+        // into a List and print the list and its size
+
+        List<Integer> list = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println("List is -> "+ list);
+        System.out.println("Size of List is -> "+ list.size());
+
+
+
+    }
+
+}
